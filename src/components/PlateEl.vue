c6e49c3cdfcf845db5fa5017888f9c1149c6826c
lllem <s.m.shemiakin@gmail.com>
2023-07-19 11:49:33 +0300

    Добавлены css-стили

--- src/components/PlateEl.vue
+++ src/components/PlateEl.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="plate-el p rounded bg-white shadow-2">
+  <div class="plate-el p rounded bg-white shadow-2 w-100">
     <slot/>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.plate-el {
+  display: inline-block;
+}
+</style>
