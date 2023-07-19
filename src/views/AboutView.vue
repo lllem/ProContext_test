9b3d6b69c3eb07156c229961250af92eb7f6aedc
lllem <s.m.shemiakin@gmail.com>
2023-07-19 12:10:28 +0300

    Добавлена информация об авторе

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <BaseLayout class="about">
-    <h1>This is an about page</h1>
+
+    <section class="p text-center">
+      <h1>Сергей Шемякин</h1>
+
+      <p><a href="mailto:s.m.shemiakin@gmail.com">s.m.shemiakin@gmail.com</a></p>
+
+      <p><a href="https://nn.hh.ru/resume/cc590818ff0c2d84270039ed1f646571577875">Резюме на HH</a></p>
+
+      <p><a href="https://www.behance.net/shem_">Портфолио</a></p>
+    </section>
+
   </BaseLayout>
 </template>
